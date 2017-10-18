edc10ee08d3f16b731f69657751090d3611147da
Chen <jiegec@qq.com>
2017-10-18 13:05:44 +0800

    Output auth result

--- auth.py
+++ auth.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python2
 import yaml
 import requests
 import hashlib
 
-login_url = "http://net.tsinghua.edu.cn/do_login.php"
+login_url = "https://net.tsinghua.edu.cn/do_login.php"
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1)" \
              " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36"
 
@@ -42,3 +43,12 @@ def go_online():
         return False
     else:
         return True
+
+if __name__ == "__main__":
+    try:
+        if go_online():
+            print("Auth succeeded")
+        else:
+            print("Auth failed")
+    except:
+        print("Auth failed")
