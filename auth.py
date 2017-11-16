f2f819d92991d0912fa98c0ecb6e97860755e46d
Jiajie Chen <jiegec@qq.com>
2017-11-16 10:07:26 +0800

    Add type annotations for auth.py

--- auth.py
+++ auth.py
@@ -2,13 +2,14 @@
 import yaml
 import requests
 import hashlib
+from typing import Tuple
 
 login_url = "https://net.tsinghua.edu.cn/do_login.php"
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1)" \
              " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36"
 
 
-def load_config(path="config/account.yaml"):
+def load_config(path:str="config/account.yaml") -> Tuple[str, str]:
     with open(path) as f:
         config = yaml.load(f)
         username = config["account"]["username"]
@@ -17,12 +18,12 @@ def load_config(path="config/account.yaml"):
         return username, password
 
 
-def hex_md5_password(password):
+def hex_md5_password(password:str) -> str:
     password = hashlib.md5(password.encode('utf-8')).hexdigest()
     return "{MD5_HEX}%s" % password
 
 
-def try_connections():
+def try_connections() -> bool:
     try:
         res = requests.get("http://www.baidu.com")
     except requests.HTTPError:
@@ -30,22 +31,22 @@ def try_connections():
     return res.status_code == 200
 
 
-def go_online():
+def go_online() -> bool:
     username, password = load_config()
-    param = {
-        "action": "login",
-        "username": username,
-        "password": hex_md5_password(password),
-        "ac_id": 1
-    }
-    res = requests.post(login_url, param)
+    param = [
+        ("action", "login"),
+        ("username", username),
+        ("password", hex_md5_password(password)),
+        ("ac_id", "1")
+    ]
+    res = requests.post(login_url, data=param)
     if res.status_code != 200:
         return False
     else:
         return True
 
 
-def main():
+def main() -> None:
     try:
         if go_online():
             print("Auth succeeded")
