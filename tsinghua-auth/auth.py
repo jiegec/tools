be745dc4bea1f37da96e83c34950b80ba2cbb27f
Jiajie Chen <jiegec@qq.com>
2018-05-07 20:01:13 +0800

    Add fff and git-pl

--- tsinghua-auth/auth.py
+++ tsinghua-auth/auth.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 import yaml
-import requests
 import hashlib
 from typing import Tuple
+
+import requests
+import logging
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+try:
+    import http.client as http_client
+except ImportError:
+    # Python 2
+    import httplib as http_client
+# http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+# logging.basicConfig()
+# logging.getLogger().setLevel(logging.DEBUG)
+# requests_log = logging.getLogger("requests.packages.urllib3")
+# requests_log.setLevel(logging.DEBUG)
+# requests_log.propagate = True
+
 
 login_url = "https://net.tsinghua.edu.cn/do_login.php"
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1)" \
